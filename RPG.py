8e6bf9b30a76658b17a9c3c7cad1a7e19bba993a
gp038341 <71278263+gp038341@users.noreply.github.com>
2020-10-01 12:25:52 -0500

    Add files via upload

--- RPG.py
+++ RPG.py
@@ -1,22 +1,56 @@
 import random
 import time
 
+OGRE = 80
+OGRE_DAMAGE = random.randint(5, 25)
+
+#text animation
 def printText(sentence):
     for char in sentence:
         print(char, end ='')
         time.sleep(.045)
     print()
 
+#Battle system
 def Battle():
-    playerHealth = 100
-    print("Player health:", playerHealth)
     print()
-    print("What would you like to do?")
-    attackChoice = input("attack, run, item")
-    damage = random.randint(1, 20)
-    if input == attack:
-        print(damage)
-    
+    playerHealth = 100
+    enemyHealth = 75
+    print("Player health:", playerHealth)
+    print("Enemy health:", enemyHealth)
+    print()
+    battling = True
+    while battling:
+        print("What would you like to do?")
+        attackChoice = input("attack, run, item >> ")
+        damage = random.randint(5, 20)
+        enemyDamage = random.randint(1, 10)
+        if attackChoice == "attack":
+            enemyHealth = enemyHealth - damage
+            print("You dealt", damage)
+            print("Enemy Health:", enemyHealth)
+            playerHealth = playerHealth - enemyDamage
+            print("Enemy dealt:", enemyDamage)
+            print("Player Health:", playerHealth)
+        if enemyHealth <= 0:
+            battling = False
+        if playerHealth <= 0:
+            print("You Lose!")
+            battling = False
+        if attackChoice == "run":
+            gotAway = random.randint(1, 50)
+            if gotAway == 1:
+                print("Couldn't get away!")
+            else:
+                print("Got away safely")
+                battling = False
+        if attackChoice == "item":
+            print("Opening inventory")
+            print(inventory)
+            item_choice = input("Which item do you want to use? >> ")
+            if item_choice == "potion":
+                playerHealth += 20
+                print("Player Health:", playerHealth)
     
 
 print("RPG")
@@ -28,16 +62,19 @@ print("Nice to meet you", name)
 
 #Story Dialogue
 printText("My name is Bob")
+time.sleep(1)
 printText("Welcome to the item mine! You can look around and find items like swords and gold.")
 printText("Go ahead and look around for some items. To move, enter n, s, e, w.")
 printText("Press l to look around.")
+time.sleep(.300)
 printText("You can only can carry 3 items in your inventory so be careful of what you choose.")
 print()
 print()
 
-items = ["a staff, " , "gold, ", "a sword, ", "a potion, ", "nothing", "nothing", "nothing", "nothing", "nothing", "nothing"]
+items = ["a staff, ", "gold, ", "a sword, ", "a potion, ", "nothing, ", "nothing, ", "nothing, ", "nothing, ", "nothing, ", "nothing, "]
 
 inventory = []
+gold = 0
 
 #Loops 3 times
 x = 0
@@ -71,26 +108,46 @@ while x < 3:
       
     if movement == "w":
         print("Going west")
+
+#Story
 print()
 print()
 printText("Bob:  Now that you have your 3 items, let's see what you got.")
 printText(inventory)
+time.sleep(2)
 printText("Wow these are all great items!")
 printText("Thanks for getting them for me!")
 list.clear(inventory)
+time.sleep(.500)
 printText("Bob ran off with all of your items.")
 print("Your current inventory:", inventory)
 
+time.sleep(1)
 printText("Hey you there!")
+time.sleep(2)
 printText("Did Bob just steal all of your items?!?")
 printText("I know a way you can get them back. Bob lives at the end of Forest Grove ")
-printText("There are many creatures along the way. Here you'll need this.")
+printText("There are many enemies along the way. Here you'll need this.")
 printText("Recieved iron sword.")
+time.sleep(1)
 printText("Names Billy by the way. Come talk to me if you ever need to heal yourself from battle")
 printText("Speaking of battle, how bout we have a practice one right now!")
 inventory.append("iron_sword")
 print("Your inventory is:", inventory)
+
 Battle()
+printText("Great battle!")
+printText("If you ever want to become a better swordsman, I can teach you and give you better swords for a price of course")
+printText("You'll want to head up north next. That will lead you to Bob")
+printText("Don't forget, press i to open your inventory")
+player_inventory = input("What would you like to do? >> ")
+if player_inventory == "i":
+    print(inventory)
+equipping_item = input("Would you like to equip an item? y / n >> ")
+if equipping_item == "y":
+    print("Equiping item")
+    damage = random.randint(7, 22)
+
 
 
 
